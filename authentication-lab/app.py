7999c9456a4697f5eb967854dca392a489af6784
AminMa23-meet <dlyzei9000@gmail.com>
2022-07-31 12:27:18 +0300

    skl

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -5,20 +5,60 @@ import pyrebase
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
+config = {
+  "apiKey": "AIzaSyDzLPjVpYpO6NOWlOy_eeJEFp2murgRo2M",
+  "authDomain": "hello-fba5c.firebaseapp.com",
+  "projectId": "hello-fba5c",
+  "storageBucket": "hello-fba5c.appspot.com",
+  "messagingSenderId": "785823535127",
+  "appId": "1:785823535127:web:623cf167fd83c52d2e8c52",
+  "measurementId": "G-6S06XY5385",
+  "databaseURL": ""
+}
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
+    error = ''
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))  
+        except:
+            error = "Authentication error"
     return render_template("signin.html")
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    error = ''
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user']=auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for('signin'))
+        except:
+            error = "Authentication error"
     return render_template("signup.html")
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
     return render_template("add_tweet.html")
+
+@app.route('/signout', methods=['GET', 'POST'])
+def signout():
+    login_session['user'] = None
+    auth.current_user = None
+    return redirect(url_for('signin'))
+
+    
+
 
 
 if __name__ == '__main__':
